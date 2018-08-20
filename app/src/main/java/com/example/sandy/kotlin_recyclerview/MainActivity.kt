62c436e0e003069b9c3c28c6a05680cb48f67aa5
sandeeptile <sandeeptile@gmail.com>
2018-08-20 15:19:10 +0530

    design done

--- app/src/main/java/com/example/sandy/kotlin_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/sandy/kotlin_recyclerview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.sandy.kotlin_recyclerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var lManager = LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL,false)
+        rview.layoutManager = lManager
+       //rview.adapter = MyAdapter(this@MainActivity)
     }
 }
